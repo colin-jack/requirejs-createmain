2bf517976c992f875e8b0569d8aaba71bbf61306
Colin Jack <colin.jack@gmail.com>
2012-08-25 15:56:31 +0100

    More work on the main file generator.

--- spec/spec.js
+++ spec/spec.js
@@ -1,28 +1,27 @@
 var vows = require('vows'),
 	assert = require('assert'),
-    underTest = require('../index');
+    underTest = require('../index'),
+    fs = require('fs');
 
 var directoryContainingJsFiles = __dirname + "/simpleRequireRoot";
 var outputFilePath = __dirname + "/main.js";
  
-vows.describe('generate require file for all files in directory').addBatch({
+vows.describe('generate require file for all files in directory (integration)').addBatch({
     'when generating main require file for a simple directory structure': {
         topic: function () { 
-            return underTest.generateFile(directoryContainingJsFiles, "*.js", outputFilePath, this.callback);
+            underTest.generateFile(directoryContainingJsFiles, "*.js", outputFilePath, this.callback);
+            return fs.readFileSync(outputFilePath);
         },
 
         'should not get an error': function (err, result) {            
-            //underTest.generateFile(directoryContainingJsFiles, "*.js", outputFilePath, this.callback);
-            // result.generateFile(directoryContainingJsFiles, "*.js", outputFilePath, this.callback);
-            //assert.isNull(err);
             assert.isNull(err);
         },
         
-        // 'the main.js starts out with a require statement': function (err) {
-            
+        'the main.js starts out with a require statement': function (err) {
+                
 
-        //     assert.equal (root(), correctReturnValue);
-        // },
+            assert.equal (root(), correctReturnValue);
+        },
 
     }
 }).run();
