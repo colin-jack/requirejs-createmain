30e4b1289a7232f1ba0fc9e929cc49eaacc9ffbd
Colin Jack <colin.jack@gmail.com>
2012-08-26 21:17:03 +0100

    Test now works, just need to make the code delete the file before it begins.

--- spec/Integration_Spec.js
+++ spec/Integration_Spec.js
@@ -28,7 +28,7 @@ vows.describe('generate require file for all files in directory (integration)').
 
         'should have created the file with expexcted content': function (err, fileContents) {
             assert.equal(fileContents, 
-                        "require(['requireSubDir1/definesFunction1.js', 'requireSubDir3/definesFunction3.js', 'requireSubDir1/requireSubDir2/definesFunction4.js', 'requireSubDir1/requireSubDir2/definesFunction2.js']);");
+                        "require(['requireSubDir1/definesFunction1.js', 'requireSubDir1/requireSubDir2/definesFunction2.js', 'requireSubDir1/requireSubDir2/definesFunction4.js', 'requireSubDir3/definesFunction3.js']);");
         }
     }
 }).run();
